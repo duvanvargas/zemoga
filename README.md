a436775f70568951e26601cd16c836fafa648b9c
mac <mac@192.168.1.102>
2020-06-10 05:05:34 -0500

    change name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Omni
+# zemoga
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.6.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('omni app is running!');
+    expect(page.getTitleText()).toEqual('zemoga app is running!');
   });
 
   afterEach(async () => {
